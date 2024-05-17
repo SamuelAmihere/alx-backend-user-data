d3252442b729d4849e5df3b3bbb4e5265104592c
Samuel Amihere <amisam2000@gmail.com>
2024-05-17 09:02:37 +0800

    0x00. Personal data: Connect to secure database

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,35 +2,14 @@
 """
 1. Regex-ing
 """
-import os
 import re
+from typing import List
+import os
 import logging
 import mysql.connector
-from typing import List
 
 
 PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
-
-
-class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class """
-
-    REDACTION = "***"
-    SEPARATOR = ";"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-
-    def __init__(self, fields: List[str]):
-        super(RedactingFormatter, self).__init__(self.FORMAT)
-        self.fields = fields
-
-    def format(self, record: logging.LogRecord) -> str:
-        """ Redacting Formatter class """
-        record.msg = filter_datum(
-            self.fields,
-            self.REDACTION,
-            record.msg,
-            self.SEPARATOR)
-        return super().format(record)
 
 
 def get_logger() -> logging.Logger:
@@ -73,3 +52,24 @@ def filter_datum(
     """returns the log message obfuscated"""
     return re.sub(extract(fields, separator),
                   replace(redaction), message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class """
+
+    REDACTION = "***"
+    SEPARATOR = ";"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ Redacting Formatter class """
+        record.msg = filter_datum(
+            self.fields,
+            self.REDACTION,
+            record.msg,
+            self.SEPARATOR)
+        return super().format(record)
