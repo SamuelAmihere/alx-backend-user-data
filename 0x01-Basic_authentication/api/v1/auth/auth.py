be0a0370a5b5dc835e671a3dee1d473922020d72
Samuel Amihere <amisam2000@gmail.com>
2024-05-22 06:30:13 +0800

    authentication

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+The authentication module.
+"""
+from flask import request
+from typing import List, TypeVar
+from models.user import User
+
+
+class Auth:
+    """The authentication class.
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """The require_auth method.
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """The authorization_header method.
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'): # type: ignore
+        """The current_user method.
+        """
+        return None
