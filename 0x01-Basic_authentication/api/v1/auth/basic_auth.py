eccae1ae7dd3ca6bf5f510739ba291a5e440a9e5
Samuel Amihere <amisam2000@gmail.com>
2024-05-22 13:26:39 +0800

    Basic authentication

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -22,17 +22,13 @@ class BasicAuth(Auth):
         return authorization_header.split(' ', 1)[1]
 
     def decode_base64_authorization_header(
-            self,
-            base64_authorization_header: str,
-            ) -> str:
-        """Decodes a base64-encoded authorization header.
+            self, base64_authorization_header: str) -> str:
+        """Decodes the Base64 Authorization Header.
         """
         if type(base64_authorization_header) == str:
             try:
-                res = base64.b64decode(
+                return base64.b64decode(
                     base64_authorization_header,
-                    validate=True,
-                )
-                return res.decode('utf-8')
+                    validate=True).decode('utf-8')
             except (binascii.Error, UnicodeDecodeError):
                 return None
