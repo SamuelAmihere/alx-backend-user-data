dde86300f0f416b2eac9bc620b8b7e939aa475b4
Samuel Amihere <amisam2000@gmail.com>
2024-05-22 12:47:08 +0800

    Basic authentication

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
 """
-This is the main entry point for the application.
+BasicAuth
 """
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """BasicAuth class
+    """
+    pass
