529ce79c5f0cf493d1fe21ac6cbabba97527cc6f
Samuel Amihere <amisam2000@gmail.com>
2024-05-24 12:25:19 +0800

    sessions

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -39,7 +39,9 @@ def login() -> Tuple[str, int]:
         return resp
     return jsonify({"error": "wrong password"}), 401
 
-@app_views.route('/auth_session/logout', methods=['DELETE'], strict_slashes=False)
+
+@app_views.route('/auth_session/logout',
+                 methods=['DELETE'], strict_slashes=False)
 def logout():
     """ DELETE handler for logout url:
     /api/v1/auth_session/logout
