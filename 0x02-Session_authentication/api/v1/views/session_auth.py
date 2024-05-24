c389ae03983d1fd21165c3990a6abd018572d568
Samuel Amihere <amisam2000@gmail.com>
2024-05-24 11:46:23 +0800

    sessions

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+""" Module of Session Authentication views
+"""
+from typing import Tuple
+import os
+from api.v1.views import app_views
+from flask import abort, jsonify, request
+from models.user import User
+from api.v1.app import auth
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def login() -> Tuple[str, int]:
+    """ POST handler for login url:
+    /api/v1/auth_session/login
+    """
+    user_email_err = {"error": "no user found for this email"}
+
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if not email or len(email.strip()) == 0:
+        return jsonify({"error": "email missing"}), 400
+    if password is None or len(password.strip()) == 0:
+        return jsonify({"error": "password missing"}), 400
+
+    try:
+        all_users = User.search({'email': email})
+    except Exception:
+        return jsonify(user_email_err), 404
+    if len(all_users) <= 0:
+        return jsonify(user_email_err), 404
+    if all_users[0].is_valid_password(password):
+        resp = jsonify(all_users[0].to_json())
+        session_id = auth.create_session(getattr(
+                            all_users[0], 'id'))
+        resp.set_cookie(os.getenv("SESSION_NAME"),
+                        session_id)
+        return resp
+    return jsonify({"error": "wrong password"}), 401
