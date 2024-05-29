e95c22da0e793e60753200bc1b710fd7d8b464f2
Samuel Amihere <amisam2000@gmail.com>
2024-05-30 07:43:40 +0800

    User authentication service

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+"""
+Hash password
+"""
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    """Hash a password
+
+    Args:
+        password (str): The password to hash
+
+    Returns:
+        bytes: The hashed password
+    """
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode(), salt)
+    return hashed_password
