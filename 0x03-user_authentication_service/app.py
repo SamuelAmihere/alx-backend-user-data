e56368cecc501aa08db40fd9cfa8d613b66ec49f
Samuel Amihere <amisam2000@gmail.com>
2024-05-30 08:07:54 +0800

    User authentication service

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+"""
+Basic Flask app
+"""
+from flask import Flask, jsonify, request, abort, redirect
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def welcome():
+    """GET route for the root URL ("/")"""
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
