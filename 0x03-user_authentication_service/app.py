a4f636a102c89a6a1cc169572bcc18b14f5493b5
Samuel Amihere <amisam2000@gmail.com>
2024-05-31 00:35:43 +0800

    Flask

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -77,5 +77,22 @@ def get_reset_password_token():
     return jsonify({"email": email, "reset_token": reset_token}), 200
 
 
+@app.route('/reset_password', methods=['PUT'])
+def update_password():
+    """PUT route for the "/reset_password" URL to update a user's
+    password"""
+    email = request.form['email']
+    reset_token = request.form['reset_token']
+    new_password = request.form['new_password']
+    try:
+        AUTH.update_password(reset_token, new_password)
+    except ValueError:
+        abort(403)
+    return jsonify({"email": email, "message": "Password updated"}), 200
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
